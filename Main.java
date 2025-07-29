906eec6b6612f977f0eea7577f9891a023874c38
LazyHC <h__chris__h@hotmail.com>
2025-07-29 21:04:39 +1200

    Creation of user input.

---
+++ Main.java
@@ -0,0 +1,20 @@
+// ===========================================
+// Course: BIT504
+// Student number: 5125361
+// Assessment 2 - Task 9
+// ===========================================
+
+// Import modules
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); // Create input
+
+        System.out.print("Please enter your name: "); // "print" instead of println ensure user data is entered on same line.
+
+        String input = scanner.nextLine(); // Get user input
+
+        System.out.println("Your name is: " + input);
+    }
+}
